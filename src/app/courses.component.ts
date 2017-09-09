ff1728ef4f59638885c81065c7f4bcff7099df02
Jack Torres <jack.torres@matrixcare.com>
2017-09-09 11:46:52 -0400

    Learn to create Pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,31 +5,15 @@ import { Component } from '@angular/core';
     selector: 'courses',
     template: `
         <h2>{{ title }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
-        <button class="btn btn-primary" 
-            [class.active]="isActive"
-            (click)="onSave($event)">Save</button>
-        <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Button</button>
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+            {{ text | summary:10 }}    
     `
 })
 export class CoursesComponent {
-    email = "me@email.com";
     title = "List of courses";
     isActive = true;
     courses;
 
-    onSave($event){
-        console.log("button was clicked", $event);
-    }
-
-    onKeyUp(){
-            console.log(this.email);
-    }
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
