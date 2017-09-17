e05603bdc87218edf9e2d9fde36357e81c5935d5
Jack Torres <jack.torres@matrixcare.com>
2017-09-17 00:42:47 -0400

    Excecise 4: Like Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
-import { FavoriteChangedEventArgs } from './favorite/favorite.component';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title: string;
-  post = {
-    title: "Title",
-    isFavorite: true
-  }
 
-  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
-    console.log("Favorite changed: ", eventArgs);
+export class AppComponent {
+  tweet = {
+      body: 'Here is the body of a tweet...',
+      tweetLikesCount: 2,
+      isLiked: false
+    }
   }
-}
